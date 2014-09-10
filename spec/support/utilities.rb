9f6291105a7495510d7b544015f82749a078bfe4
mizoguchi <mizoguchi.y@gmail.com>
2014-09-10 12:56:09 +0900

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,3 +7,18 @@ def full_title(page_title)
   end
 end
   
+include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+Rspec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+
